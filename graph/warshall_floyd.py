20e4ab54a01a28766e759ba8bfd7a1145333b855
藤平燎 <fujihiraryo@team-lab.com>
2020-03-30 20:01:23 +0900

    add geometry

---
+++ graph/warshall_floyd.py
@@ -0,0 +1,19 @@
+def warshall_floyd(G):
+    inf = 10**18
+    # グラフGに対してすべての頂点対の距離を求める
+    n = len(G)
+    d = [[inf for i in range(n)] for j in range(n)]
+    for i in range(n):
+        d[i][i] = 0
+        for j in G[i].keys():
+            d[i][j] = G[i][j]
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                d[i][j] = min(d[i][j], d[i][k]+d[k][j])
+    return d
+
+
+# テスト
+G = [{1: 5, 2: 1}, {0: 5, 2: 1}, {0: 1, 1: 1}, {}]
+print(warshall_floyd(G))
