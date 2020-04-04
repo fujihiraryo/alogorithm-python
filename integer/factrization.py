47defc4eb2f02763f4bc11fd845252118868e783
藤平燎 <fujihiraryo@team-lab.com>
2020-04-05 08:23:48 +0900

    試し割りを追加

--- integer/factrization.py
+++ integer/factrization.py
@@ -1,10 +1,29 @@
+def factrize(x):
+    # 試し割りによるxの素因数分解
+    f = {}
+    tmp = x
+    i = 2
+    while i**2 <= tmp:
+        cnt = 0
+        while tmp % i == 0:
+            cnt += 1
+            tmp = tmp//i
+        if cnt > 0:
+            f[i] = cnt
+        i += 1
+    if tmp != 1 or f == {}:
+        f[tmp] = 1
+    return f
+
+
 class Sieve():
-    # 最初にn以下の素数を列挙する
-    # n=10^5くらいまでなら余裕
+    # エラトステネスの篩
     def __init__(self, n):
+        # f[x]=0ならxは素数
+        # f[x]はxの最小の素因数
         X = list(range(2, n + 1))
         f = {}
-        while X[0] <= n**0.5:
+        while X[0]**2 <= n:
             tmp = X[0]
             f[tmp] = 0
             X_new = []
@@ -18,9 +37,10 @@ class Sieve():
             f[x] = 0
         self.primes = f
 
-    def factrize(self, n):
+    def factrize(self, x):
+        # n未満のxを素因数分解
         f = self.primes
-        tmp = n
+        tmp = x
         g = {}
         while f[tmp]:
             try:
@@ -36,8 +56,12 @@ class Sieve():
 
 
 # テスト
+print(Sieve(100).primes)
 n = 10**5
 sieve = Sieve(n)
 print(sieve.factrize(3664))
+print(factrize(3664))
 print(sieve.factrize(28781))
+print(factrize(28781))
 print(sieve.factrize(10001))
+print(factrize(11223344556677889900))
