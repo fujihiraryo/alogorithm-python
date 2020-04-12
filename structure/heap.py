51ea58fa1cd2544dc4748a41a6426ac8f0f67607
藤平燎 <fujihiraryo@team-lab.com>
2020-04-12 16:04:32 +0900

    最大版追加

--- structure/heap.py
+++ structure/heap.py
@@ -1,38 +1,45 @@
-class Heap(list):
+class Heap():
+    def __init__(self, A, reverse=False):
+        if reverse:
+            self.sgn = -1
+            self.A = sorted([-a for a in A])
+        else:
+            self.sgn = 1
+            self.A = sorted(A)
+
     def push(self, x):
-        i = len(self)
-        print(i)
-        self.append(x)
+        i = len(self.A)
+        self.A.append(x*self.sgn)
         while i > 0:
             p = (i-1)//2
-            if self[p] <= x:
+            if self.A[p] <= x*self.sgn:
                 break
-            self[i], self[p] = self[p], self[i]
+            self.A[i], self.A[p] = self.A[p], self.A[i]
             i = p
 
-    def hpop(self):
-        x = self[0]
-        y = self[-1]
+    def pop(self):
+        x = self.A[0]
+        y = self.A[-1]
         i = 0
-        while 2*i+1 < len(self):
+        while 2*i+1 < len(self.A):
             a, b = 2*i+1, 2*i+2
-            if b < len(self) and self[b] < self[a]:
+            if b < len(self.A) and self.A[b] < self.A[a]:
                 a = b
-            if self[a] >= y:
+            if self.A[a] >= y:
                 break
-            self[i] = self[a]
+            self.A[i] = self.A[a]
             i = a
-        self[i] = y
-        self.pop()
-        return x
+        self.A[i] = y
+        self.A.pop()
+        return x*self.sgn
 
     def top(self):
-        return self[0]
+        return self.A[0]*self.sgn
 
 
 # テスト
-Q = [2, 3, 4, 5]
-HQ = Heap(Q)
-print(HQ.hpop())
-HQ.push(1)
+Q = [3, 5, 2, 4]
+HQ = Heap(Q, reverse=True)
+print(HQ.A)
+HQ.push(6)
 print(HQ.top())
