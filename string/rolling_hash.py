a4cc4063007bb6e72f9863dafdbcded3b9e14ed5
藤平燎 <fujihiraryo@team-lab.com>
2020-04-15 17:53:47 +0900

    文字列の出現回数カウント

--- string/rolling_hash.py
+++ string/rolling_hash.py
@@ -8,22 +8,22 @@ class HashString:
 
     def pop_left(self):
         S, l, r, b, p, h = self.S, self.l, self.r, self.b, self.p, self.hash
-        self.hash = (h-ord(S[l])*pow(b, r-l-1, p)) % p
+        self.hash = (h - ord(S[l]) * pow(b, r-l-1, p)) % p
         self.l += 1
 
     def pop_right(self):
         S, l, r, b, p, h = self.S, self.l, self.r, self.b, self.p, self.hash
-        self.hash = ((h-ord(S[r-1])) * pow(b, p-2, p)) % p
+        self.hash = ((h - ord(S[r-1])) * pow(b, p-2, p)) % p
         self.r -= 1
 
     def push_left(self):
         S, l, r, b, p, h = self.S, self.l, self.r, self.b, self.p, self.hash
-        self.hash = (h+ord(S[l-1])*pow(b, r-l, p)) % p
+        self.hash = (h + ord(S[l-1]) * pow(b, r-l, p)) % p
         self.l -= 1
 
     def push_right(self):
         S, l, r, b, p, h = self.S, self.l, self.r, self.b, self.p, self.hash
-        self.hash = (h*b+ord(S[r])) % p
+        self.hash = (h * b + ord(S[r])) % p
         self.r += 1
 
     def shift_left(self):
@@ -42,32 +42,29 @@ def contain(S0, S1):
         return False
     hS0 = HashString(S0, 0, n0)
     hS1 = HashString(S1, 0, n0)
-    if hS0.hash == hS1.hash:
-        return True
-    for i in range(n1-n0):
-        hS1.shift_right()
+    for i in range(n1-n0+1):
         if hS0.hash == hS1.hash:
             return True
+        try:
+            hS1.shift_right()
+        except:
+            pass
     return False
 
 
-S = 'abcde'
-hS = HashString(S, 1, 2)
-print(hS.hash)
-hS.push_right()
-print(hS.hash)
-hS.pop_right()
-print(hS.hash)
-hS.push_left()
-print(hS.hash)
-hS.pop_left()
-print(hS.hash)
-print(hS.r-hS.l)
-
-S0 = 'abcd'
-S1 = 'xxxabcdxx'
-print(contain(S0, S1))
-
-S0 = 'abcde'
-S1 = 'xxxxabcdxeeee'
-print(contain(S0, S1))
+def count(S0, S1):
+    # S0がS1に現れる回数を数える
+    n0, n1 = len(S0), len(S1)
+    if n0 > n1:
+        return 0
+    hS0 = HashString(S0, 0, n0)
+    hS1 = HashString(S1, 0, n0)
+    cnt = 0
+    for i in range(n1-n0+1):
+        if hS0.hash == hS1.hash:
+            cnt += 1
+        try:
+            hS1.shift_right()
+        except:
+            pass
+    return cnt
