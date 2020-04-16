372458103de5145bc94f2249bc5c038375ce3775
藤平燎 <fujihiraryo@team-lab.com>
2020-04-17 03:53:30 +0900

    変数名の変更

--- graph/warshall_floyd.py
+++ graph/warshall_floyd.py
@@ -2,16 +2,16 @@ def warshall_floyd(G):
     inf = 10**18
     # 重みつきグラフGに対してすべての頂点対の距離を求める
     n = len(G)
-    d = [[inf for i in range(n)] for j in range(n)]
+    D = [[inf for i in range(n)] for j in range(n)]
     for i in range(n):
-        d[i][i] = 0
+        D[i][i] = 0
         for j in G[i].keys():
-            d[i][j] = G[i][j]
+            D[i][j] = G[i][j]
     for k in range(n):
         for i in range(n):
             for j in range(n):
-                d[i][j] = min(d[i][j], d[i][k]+d[k][j])
-    return d
+                D[i][j] = min(D[i][j], D[i][k]+D[k][j])
+    return D
 
 
 # テスト
