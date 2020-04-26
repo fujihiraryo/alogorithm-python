ffd2d62638080ca370272b4cbc6cad89edeb3bef
藤平燎 <fujihiraryo@team-lab.com>
2020-04-27 07:39:52 +0900

    Heapにkeyを指定できるようにした

--- structure/heap.py
+++ structure/heap.py
@@ -1,21 +1,19 @@
 class Heap():
-    def __init__(self, A, reverse=False):
-        if reverse:
-            self.sgn = -1
-            self.A = sorted([-a for a in A])
-        else:
-            self.sgn = 1
-            self.A = sorted(A)
+    def __init__(self, A, key=lambda x: x):
+        self.f = key
+        self.A = sorted(A, key=key)
+        self.top = self.A[0]
 
     def push(self, x):
         i = len(self.A)
-        self.A.append(x*self.sgn)
+        self.A.append(x)
         while i > 0:
             p = (i-1)//2
-            if self.A[p] <= x*self.sgn:
+            if self.f(self.A[p]) <= self.f(x):
                 break
             self.A[i], self.A[p] = self.A[p], self.A[i]
             i = p
+        self.top = self.A[0]
 
     def pop(self):
         x = self.A[0]
@@ -23,23 +21,22 @@ class Heap():
         i = 0
         while 2*i+1 < len(self.A):
             a, b = 2*i+1, 2*i+2
-            if b < len(self.A) and self.A[b] < self.A[a]:
+            if b < len(self.A) and self.f(self.A[b]) < self.f(self.A[a]):
                 a = b
-            if self.A[a] >= y:
+            if self.f(self.A[a]) >= self.f(y):
                 break
             self.A[i] = self.A[a]
             i = a
         self.A[i] = y
         self.A.pop()
-        return x*self.sgn
-
-    def top(self):
-        return self.A[0]*self.sgn
+        self.top = self.A[0]
+        return x
 
 
 # テスト
-Q = [3, 5, 2, 4]
-HQ = Heap(Q, reverse=True)
-print(HQ.A)
-HQ.push(6)
-print(HQ.top())
+Q = [(1, 2), (2, 3)]
+HQ = Heap(Q, key=lambda x: sum(x))
+print(HQ.top)
+HQ.push((1, 3))
+HQ.pop()
+print(HQ.top)
