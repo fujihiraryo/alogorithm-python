c742936c4c5cae57b6deaa3e8083f1931de7f022
藤平燎 <fujihiraryo@team-lab.com>
2020-10-12 00:00:51 +0900

    多項式を修正

--- integer/polynomial.py
+++ integer/polynomial.py
@@ -1,38 +1,29 @@
 class Polynomial:
-    def __init__(self, coefficients=[1], MOD=10 ** 9 + 7):
+    def __init__(self, coefficients, zero=0):
         self.coefficients = coefficients
-        self.MOD = MOD
+        self.zero = zero
 
     def substitute(self, x):
-        y = 0
+        y = self.zero
         for i, a in enumerate(self.coefficients):
-            y += a * x ** i
-            y %= self.MOD
+            y = y + a * x ** i
         return y
 
     def __mul__(self, polynomial):
         m = len(self.coefficients)
         n = len(polynomial.coefficients)
-        coefficients = [0] * (m + n - 1)
+        coefficients = [self.zero] * (m + n - 1)
         for i, a in enumerate(self.coefficients):
             for j, b in enumerate(polynomial.coefficients):
-                coefficients[i + j] += a * b
-                coefficients[i + j] %= self.MOD
+                coefficients[i + j] = coefficients[i + j] + a * b
         return Polynomial(coefficients)
 
     def antiderivative(self):
-        coefficients = [0] * (len(self.coefficients) + 1)
+        coefficients = [self.zero] * (len(self.coefficients) + 1)
         for i, a in enumerate(self.coefficients):
-            coefficients[i + 1] = a * pow(i + 1, self.MOD - 2, self.MOD)
-            coefficients[i + 1] %= self.MOD
+            coefficients[i + 1] = a / (i + 1)
         return Polynomial(coefficients)
 
     def integrate(self, a, b):
         P = self.antiderivative()
-        return (P.substitute(b) - P.substitute(a)) % self.MOD
-
-
-if __name__ == "__main__":
-    p = Polynomial([1, 1, 1])
-    print(p.substitute(2))
-    print(p.integrate(0, 6))
+        return P.substitute(b) - P.substitute(a)
