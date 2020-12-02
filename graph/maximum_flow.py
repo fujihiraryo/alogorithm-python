62c606a11d65c53b89cde980dad3fec9e20c7516
fujihiraryo <fujihiraryo@fujihiraryos-Mac-mini.local>
2020-12-03 04:00:26 +0900

    change parent to last

--- graph/maximum_flow.py
+++ graph/maximum_flow.py
@@ -6,7 +6,7 @@ def maximum_flow(graph, start, goal):
         # find path
         visited = [0] * n
         stack = [start]
-        parent = [None] * n
+        last = [None] * n
         while stack:
             x = stack.pop()
             visited[x] = 1
@@ -16,20 +16,20 @@ def maximum_flow(graph, start, goal):
                 if visited[y] or graph[x][y] == 0:
                     continue
                 stack.append(y)
-                parent[y] = x
+                last[y] = x
         if x != goal:
             break
         # min capacity
         y = goal
         min_capacity = INF
-        while parent[y] is not None:
-            x = parent[y]
+        while last[y] is not None:
+            x = last[y]
             min_capacity = min(min_capacity, graph[x][y])
             y = x
         # update graph
         y = goal
-        while parent[y] is not None:
-            x = parent[y]
+        while last[y] is not None:
+            x = last[y]
             graph[x][y] -= min_capacity
             graph[y][x] += min_capacity
             y = x
