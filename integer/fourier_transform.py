985e0cb1e2f1eaebe98625f6e0bf3a78314a7752
藤平燎 <fujihiraryo@team-lab.com>
2020-12-16 18:50:14 +0900

    refactor

--- integer/fourier_transform.py
+++ integer/fourier_transform.py
@@ -8,45 +8,30 @@ class FFT:
         self.rn = pow(n, p - 2, p)
         self.rw = pow(w, p - 2, p)
 
-    def base(self, A, n, w):
+    def base(self, a, n, w):
         if n == 1:
-            return A
+            return a
         p = self.p
-        for i in range(n - len(A)):
-            A.append(0)
-        F0 = self.base(A[0::2], n // 2, pow(w, 2, p))
-        F1 = self.base(A[1::2], n // 2, pow(w, 2, p))
+        for i in range(n - len(a)):
+            a.append(0)
+        f0 = self.base(a[0::2], n // 2, pow(w, 2, p))
+        f1 = self.base(a[1::2], n // 2, pow(w, 2, p))
         x = 1
-        F = []
+        f = []
         for i in range(n):
-            f = (F0[i % (n // 2)] + x * F1[i % (n // 2)]) % p
+            f.append((f0[i % (n // 2)] + x * f1[i % (n // 2)]) % p)
             x = (x * w) % p
-            F.append(f)
-        return F
+        return f
 
-    def fft(self, A):
-        # 順変換
-        return self.base(A, self.n, self.w)
+    def fft(self, a):
+        return self.base(a, self.n, self.w)
 
-    def ift(self, A):
-        # 逆変換
-        F = self.base(A, self.n, self.rw)
-        return [(f * self.rn) % self.p for f in F]
+    def ift(self, a):
+        f = self.base(a, self.n, self.rw)
+        return list(map(lambda x: (x * self.rn) % self.p, f))
 
-    def conv(self, A, B):
-        # AとBの畳み込み
-        nA, nB = len(A), len(B)
-        nC = nA + nB - 1
-        FA = self.fft(A)
-        FB = self.fft(B)
-        FC = [FA[i] * FB[i] for i in range(self.n)]
-        C = self.ift(FC)
-        return C[:nC]
-
-
-# テスト
-A = [0, 1, 2, 3, 4]
-B = [0, 1, 2, 4, 8]
-fft = FFT(262144, 347078657, 13)
-C = fft.conv(A, B)
-print(C)
+    def conv(self, a, b):
+        fa = self.fft(a)
+        fb = self.fft(b)
+        fc = [fa[i] * fb[i] for i in range(self.n)]
+        return self.ift(fc)
