fa0af1f3b952651556bf7687641a886469980506
fujihiraryo <fujihiraryo@team-lab.com>
2021-01-10 13:47:31 +0900

    Update union_find.py

--- structure/union_find.py
+++ structure/union_find.py
@@ -1,6 +1,8 @@
 class UnionFind:
     def __init__(self, size):
         self.parent = list(range(size))
+        self.group_size = [1] * size
+        self.unite_count = [0] * size
 
     def find(self, i):
         j = i
@@ -12,4 +14,8 @@ class UnionFind:
         i0, j0 = self.find(i), self.find(j)
         if i0 > j0:
             i0, j0 = j0, i0
+        self.unite_count[i0] += 1
         self.parent[j0] = i0
+        if i0 != j0:
+            self.unite_count[i0] += self.unite_count[j0]
+            self.group_size[i0] += self.group_size[j0]
