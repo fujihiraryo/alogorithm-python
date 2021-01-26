fc4017276ee520844da313fc3d78556b4c660e87
fujihiraryo <fujihiraryo.team-lab.com>
2021-01-27 04:09:20 +0900

    add RangeBIT

--- structure/fenwick_tree.py
+++ structure/fenwick_tree.py
@@ -1,4 +1,4 @@
-class FenwickTree:
+class BIT:
     def __init__(self, n):
         self.size = n
         self.arr = [0] * (n + 1)
@@ -19,3 +19,20 @@ class FenwickTree:
         while tmp <= self.size:
             self.arr[tmp] += x
             tmp += tmp & -tmp
+
+
+class RangeBIT:
+    def __init__(self, n):
+        self.bit0 = BIT(n)
+        self.bit1 = BIT(n)
+
+    def add(self, i, j, x):
+        self.bit0.add(i, -x * i)
+        self.bit0.add(j, x * j)
+        self.bit1.add(i, x)
+        self.bit1.add(j, -x)
+
+    def sum(self, i, j):
+        si = self.bit0.sum(i) + self.bit1.sum(i) * i
+        sj = self.bit0.sum(j) + self.bit1.sum(j) * j
+        return sj - si
