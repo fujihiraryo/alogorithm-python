8ec2913a91feeb883e1de957694c28aca992b54e
fujihiraryo <fujihiraryo.team-lab.com>
2021-01-27 21:03:22 +0900

    sparse table

--- README.md
+++ README.md
@@ -65,4 +65,5 @@
 | [structure/ordered_set.py](structure/ordered_set.py)       | 順序付き集合 |
 | [structure/priority_quene.py](structure/priority_quene.py)       | 優先度付きキュー |
 | [structure/segment_tree.py](structure/segment_tree.py)       | セグメント木 |
+| [structure/sparse_table.py](structure/sparse_table.py)       | スパーステーブル |
 | [structure/union_find.py](structure/union_find.py) | Union Find   |
---
+++ structure/sparse_table.py
@@ -0,0 +1,26 @@
+class SparseTable:
+    def __init__(self, a):
+        n = len(a)
+        table = [[a[i]] for i in range(n)]
+        for k in range(n.bit_length()):
+            for i in range(n):
+                if i + 2 ** (k + 1) > n:
+                    continue
+                x = table[i][k]
+                y = table[i + 2 ** k][k]
+                table[i].append(min(x, y))
+        self.table = table
+        self.bit_length = [i.bit_length() for i in range(n + 1)]
+
+    def range(self, i, j):
+        k = self.bit_length[j - i] - 1
+        return min(self.table[i][k], self.table[j - 2 ** k][k])
+
+
+n, k = map(int, input().split())
+(*a,) = map(int, input().split())
+st = SparceTable(a)
+ans = []
+for i in range(n - k + 1):
+    ans.append(st.range(i, i + k))
+print(*ans)
