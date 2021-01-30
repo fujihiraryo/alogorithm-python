72c38ba6b609564c1949aa3d60141f73c7918465
fujihiraryo <fujihiraryo.team-lab.com>
2021-01-30 20:55:41 +0900

    重み付きUnionFind

--- structure/union_find.py
+++ structure/union_find.py
@@ -7,6 +7,7 @@ class UnionFind:
         j = i
         while self.parent[j] != j:
             j = self.parent[j]
+            self.parent[i] = j
         return j
 
     def unite(self, i, j):
@@ -16,3 +17,33 @@ class UnionFind:
         self.parent[j0] = i0
         if i0 != j0:
             self.group_size[i0] += self.group_size[j0]
+
+
+class WeightedUnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.weight = [0] * n
+
+    def find(self, i):
+        j = i
+        while self.parent[j] != j:
+            j = self.parent[j]
+            self.parent[i] = j
+            self.weight[i] += self.weight[j]
+        return j
+
+    def diff(self, i, j):
+        if self.find(i) != self.find(j):
+            return None
+        return self.weight[j] - self.weight[i]
+
+    def unite(self, i, j, w):
+        i0, j0 = self.find(i), self.find(j)
+        w0 = w + self.weight[i] - self.weight[j]
+        if i0 == j0:
+            return
+        if i0 > j0:
+            i0, j0 = j0, i0
+            w0 = -w0
+        self.parent[j0] = i0
+        self.weight[j0] = w0
