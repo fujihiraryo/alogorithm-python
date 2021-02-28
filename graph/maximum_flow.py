339053620f2fa0bff673665faf3eb3e7ea40a328
fujihiraryo <fujihiraryo.team-lab.com>
2021-03-01 04:30:49 +0900

    最大流をDinic法に修正

--- graph/maximum_flow.py
+++ graph/maximum_flow.py
@@ -1,39 +1,46 @@
-INF = 1 << 30
+class Dinic:
+    def __init__(self, graph, start, goal, INF=1 << 30):
+        n = len(graph)
+        self.size = n
+        self.graph = graph
+        self.start = start
+        self.goal = goal
+        self.INF = INF
 
-
-def maximum_flow(graph, start, goal):
-    n = len(graph)
-    max_flow = 0
-    while True:
-        # find path
-        visited = [0] * n
-        stack = [start]
-        last = [None] * n
-        while stack:
-            x = stack.pop()
-            visited[x] = 1
-            if x == goal:
-                break
-            for y in graph[x].keys():
-                if visited[y] or graph[x][y] == 0:
+    def bfs(self):
+        self.dist = [self.INF] * self.size
+        self.dist[self.start] = 0
+        queue = [self.start]
+        for x in queue:
+            for y in self.graph[x]:
+                if self.graph[x][y] == 0 or self.dist[y] < self.INF:
                     continue
-                stack.append(y)
-                last[y] = x
-        if x != goal:
-            break
-        # min capacity
-        y = goal
-        min_capacity = INF
-        while last[y] is not None:
-            x = last[y]
-            min_capacity = min(min_capacity, graph[x][y])
-            y = x
-        # update graph
-        y = goal
-        while last[y] is not None:
-            x = last[y]
-            graph[x][y] -= min_capacity
-            graph[y][x] += min_capacity
-            y = x
-        max_flow += min_capacity
-    return max_flow
+                self.dist[y] = self.dist[x] + 1
+                queue.append(y)
+
+    def dfs(self, x, flow):
+        if x == self.goal:
+            return flow
+        for y in self.graph[x]:
+            capa = self.graph[x][y]
+            if capa == 0 or self.dist[x] >= self.dist[y] or y in self.checked[x]:
+                continue
+            self.checked[x].add(y)
+            f = self.dfs(y, min(flow, capa))
+            if f:
+                self.graph[x][y] -= f
+                self.graph[y][x] += f
+                return f
+        return 0
+
+    def max_flow(self):
+        res = 0
+        while True:
+            self.bfs()
+            if self.dist[self.goal] == self.INF:
+                return res
+            flow = self.INF
+            self.checked = [set() for _ in range(self.size)]
+            while flow:
+                flow = self.dfs(self.start, self.INF)
+                res += flow
