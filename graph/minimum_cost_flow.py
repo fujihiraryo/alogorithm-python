74632a84c87fa6f9a9cd1804d02048efe5b5a2f9
fujihiraryo <fujihiraryo.team-lab.com>
2021-03-17 07:17:21 +0900

    update min_cost_flow

--- graph/minimum_cost_flow.py
+++ graph/minimum_cost_flow.py
@@ -1,8 +1,9 @@
-INF = 1 << 30
-
-
-def minimum_cost_flow(graph, start, goal, flow):
+def min_cost_flow(graph, start, goal, flow, INF=1 << 30):
     n = len(graph)
+    residual_graph = [{} for _ in range(n)]
+    for x in range(n):
+        for y in graph[x]:
+            residual_graph[x][y] = graph[x][y]
     residual_flow = flow
     min_cost = 0
     while residual_flow:
@@ -16,8 +17,8 @@ def minimum_cost_flow(graph, start, goal, flow):
             for i in range(n):
                 if path_cost[i] == INF:
                     continue
-                for j in graph[i].keys():
-                    capa, cost = graph[i][j]
+                for j in residual_graph[i].keys():
+                    capa, cost = residual_graph[i][j]
                     if capa > 0 and path_cost[j] > path_cost[i] + cost:
                         path_cost[j] = path_cost[i] + cost
                         last[j] = i
@@ -28,7 +29,7 @@ def minimum_cost_flow(graph, start, goal, flow):
         min_capa = residual_flow
         x = goal
         while x != start:
-            capa, _ = graph[last[x]][x]
+            capa, _ = residual_graph[last[x]][x]
             min_capa = min(min_capa, capa)
             x = last[x]
         residual_flow -= min_capa
@@ -36,9 +37,9 @@ def minimum_cost_flow(graph, start, goal, flow):
         # update graph
         x = goal
         while x != start:
-            capa, cost = graph[last[x]][x]
-            graph[last[x]][x] = (capa - min_capa, cost)
-            capa, cost = graph[x][last[x]]
-            graph[x][last[x]] = (capa + min_capa, cost)
+            capa, cost = residual_graph[last[x]][x]
+            residual_graph[last[x]][x] = (capa - min_capa, cost)
+            capa, cost = residual_graph[x][last[x]]
+            residual_graph[x][last[x]] = (capa + min_capa, cost)
             x = last[x]
     return min_cost
