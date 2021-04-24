5fe0ff8243c38ab93bb9873128ece9213dfbd059
fujihiraryo <fujihiraryo@team-lab.com>
2021-04-25 05:16:21 +0900

    Update union_find.py

--- structure/union_find.py
+++ structure/union_find.py
@@ -1,22 +1,25 @@
 class UnionFind:
     def __init__(self, n):
         self.parent = list(range(n))
-        self.group_size = [1] * n
+        self.count = n
 
     def find(self, i):
-        j = i
-        while self.parent[j] != j:
-            j = self.parent[j]
-            self.parent[i] = j
-        return j
+        if self.parent[i] == i:
+            return i
+        self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
 
     def unite(self, i, j):
-        i0, j0 = self.find(i), self.find(j)
-        if i0 > j0:
-            i0, j0 = j0, i0
-        self.parent[j0] = i0
-        if i0 != j0:
-            self.group_size[i0] += self.group_size[j0]
+        i, j = self.find(i), self.find(j)
+        if i == j:
+            return
+        if i > j:
+            i, j = j, i
+        self.parent[j] = i
+        self.count -= 1
+
+    def same(self, i, j):
+        return self.find(i) == self.find(j)
 
 
 class WeightedUnionFind:
