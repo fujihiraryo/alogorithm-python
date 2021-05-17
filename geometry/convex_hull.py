a4a899ccfbf23a553c62ee74c3a55656337da179
fujihiraryo <fujihiraryo@team-lab.com>
2021-05-18 00:50:42 +0900

    Update convex_hull.py

--- geometry/convex_hull.py
+++ geometry/convex_hull.py
@@ -1,30 +1,25 @@
-def counter_clock(a, b, p):
+def clockwise(a, b, p):
     ax, ay = a
     bx, by = b
     px, py = p
-    return (bx - ax) * (py - ay) > (by - ay) * (px - ax)
+    return (bx - ax) * (py - ay) < (by - ay) * (px - ax)
 
 
-def convex_hull(point_list):
-    point_list.sort()
-    upper_list = []
-    for i, p in enumerate(point_list):
-        if i < 2:
-            upper_list.append(p)
-            continue
-        a, b = upper_list[-2], upper_list[-1]
-        while counter_clock(a, b, p) and len(upper_list) > 2:
-            upper_list.pop()
-            a, b = upper_list[-2], upper_list[-1]
-        upper_list.append(p)
-    lower_list = []
-    for i, p in enumerate(lower_list):
-        if i < 2:
-            lower_list.append(p)
-            continue
-        a, b = lower_list[-2], lower_list[-1]
-        while not counter_clock(a, b, p) and len(lower_list) > 2:
-            lower_list.pop()
-            a, b = lower_list[-2], lower_list[-1]
-        lower_list.append(p)
-    return set(upper_list + lower_list)
+def convex_hull(pl):
+    pl.sort()
+    n = len(pl)
+    ul = []
+    ul.append(pl[0])
+    ul.append(pl[1])
+    for i in range(2, n):
+        while len(ul) >= 2 and not clockwise(ul[-2], ul[-1], pl[i]):
+            ul.pop()
+        ul.append(pl[i])
+    ll = []
+    ll.append(pl[-1])
+    ll.append(pl[-2])
+    for i in range(n - 2)[::-1]:
+        while len(ll) >= 2 and not clockwise(ll[-2], ll[-1], pl[i]):
+            ll.pop()
+        ll.append(pl[i])
+    return ul + ll[1:-1]
