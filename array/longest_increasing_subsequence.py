1e52bbaeabee7fb9ca33611dcd4dba857c1e7517
fujihiraryo <fujihiraryo@team-lab.com>
2023-11-21 22:01:59 +0900

    Update longest_increasing_subsequence.py

--- array/longest_increasing_subsequence.py
+++ array/longest_increasing_subsequence.py
@@ -2,12 +2,11 @@ import bisect
 
 
 def lis(a):
-    n = len(a)
     b = []
-    for i in range(n):
-        j = bisect.bisect_left(b, a[i])
-        if j == n:
-            b.append(a[i])
+    for x in a:
+        i = bisect.bisect_left(b, x)
+        if i == len(b):
+            b.append(x)
         else:
-            b[j] = a[i]
-    return b
+            b[i] = x
+    return len(b)
